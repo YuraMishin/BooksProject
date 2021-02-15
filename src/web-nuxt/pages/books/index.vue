6f993a2303438a9ddbaa6459a6f03722f60116e4
Mishin Yura <mishin.inbox@gmail.com>
2021-02-15 11:58:08 +0300

    Added a page to display all the books. Closes #29

---
+++ src/web-nuxt/pages/books/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="books-page">
+    <BookList :books="loadedBooks"/>
+  </div>
+</template>
+
+<script>
+  import BookList from "@/components/Books/BookList";
+
+  export default {
+    components: {
+      BookList
+    },
+    computed: {
+      loadedBooks() {
+        return this.$store.getters.loadedBooks
+      }
+    }
+  };
+</script>
+
+<style scoped>
+  .books-page {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
