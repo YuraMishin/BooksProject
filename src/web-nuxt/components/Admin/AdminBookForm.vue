640540b4f40e998a58be70e3591c1f805550c6e9
Mishin Yura <mishin.inbox@gmail.com>
2021-02-15 18:51:57 +0300

    Added mocked UI to edit the specific book. Closes #33

---
+++ src/web-nuxt/components/Admin/AdminBookForm.vue
@@ -0,0 +1,49 @@
+<template>
+  <form @submit.prevent="onSave">
+    <AppControlInput v-model="editedBook.title">Title</AppControlInput>
+    <AppButton type="submit">Save</AppButton>
+    <AppButton
+      type="button"
+      style="margin-left: 10px"
+      btn-style="cancel"
+      @click="onCancel">Cancel
+    </AppButton>
+  </form>
+</template>
+
+<script>
+  import AppControlInput from "@/components/UI/AppControlInput";
+  import AppButton from "@/components/UI/AppButton";
+
+  export default {
+    components: {
+      AppControlInput,
+      AppButton
+    },
+    props: {
+      book: {
+        type: Object,
+        required: false
+      }
+    },
+    data() {
+      return {
+        editedBook: this.book
+          ? {...this.book}
+          : {
+            title: ""
+          }
+      };
+    },
+    methods: {
+      onSave() {
+        // Save the book
+        console.log(this.editedBook);
+      },
+      onCancel() {
+        // Navigate back
+        this.$router.push("/admin");
+      }
+    }
+  };
+</script>
---
+++ src/web-nuxt/pages/admin/_bookId/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="admin-book-page">
+    <section class="update-form">
+      <AdminBookForm :book="loadedBook"/>
+    </section>
+  </div>
+</template>
+
+<script>
+  import AdminBookForm from '@/components/Admin/AdminBookForm'
+
+  export default {
+    layout: 'admin',
+    components: {
+      AdminBookForm
+    },
+    data() {
+      return {
+        loadedBook: {
+          title: 'Book'
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .update-form {
+    width: 90%;
+    margin: 20px auto;
+  }
+
+  @media (min-width: 768px) {
+    .update-form {
+      width: 500px;
+    }
+  }
+</style>
