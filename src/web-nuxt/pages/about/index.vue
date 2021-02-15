24f481e14a40b6916aed2d87098dd5cc3b94f980
Mishin Yura <mishin.inbox@gmail.com>
2021-02-15 12:08:48 +0300

    Added a page to display about info. Closes #31

---
+++ src/web-nuxt/pages/about/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="about-page">
+    <h1>Books Project</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab aliquid animi,
+      architecto asperiores blanditiis dignissimos, dolores esse eveniet facilis ipsa,
+      maxime mollitia odio officia optio pariatur quidem sed vitae voluptas.</p>
+  </div>
+</template>
+
+<style scoped>
+  .about-page {
+    text-align: center;
+    padding: 30px;
+  }
+</style>
