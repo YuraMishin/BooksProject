1f140e51211051a5c994c24dd61e77a5e601fe06
Mishin Yura <mishin.inbox@gmail.com>
2021-03-09 17:17:49 +0300

    Enhanced index page. Closes #81

--- src/web-nuxt/nuxt.config.js
+++ src/web-nuxt/nuxt.config.js
@@ -4,7 +4,7 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - Books Project',
-    title: '',
+    title: 'Welcome',
     htmlAttrs: {
       lang: 'en'
     },
--- src/web-nuxt/pages/v2/index.vue
+++ src/web-nuxt/pages/v2/index.vue
@@ -1,12 +1,23 @@
 <template>
   <div>
     <h1>Books</h1>
-    <div v-if="books">
-      <div
-        style="margin: 10px"
-        v-for="b in books"
-      >
-        <v-btn :to="`/v2/book/${b.id}`">{{b.title}}</v-btn>
+    <div>
+      <div v-for="(s, index) in sections" :key="index">
+        <div class="d-flex flex-column align-center">
+          <p class="text-h5">
+            {{s.title}}
+          </p>
+          <div>
+            <v-btn
+              class="mx-1"
+              v-for="(item, index) in s.collection"
+              :key="index"
+              :to="s.routeFactory(item.id)">
+              {{item.title ? item.title : item.name}}
+            </v-btn>
+          </div>
+        </div>
+        <v-divider class="my-5"></v-divider>
       </div>
     </div>
   </div>
@@ -18,7 +29,32 @@
   export default {
     name: "index",
     layout: 'index2Layout',
-    computed: mapState('books', ['books'])
+    computed: {
+      ...mapState('books', [
+        'books',
+        'categories',
+        'difficulties'
+      ]),
+      sections() {
+        return [
+          {
+            collection: this.books,
+            title: "Books",
+            routeFactory: (id) => `/v2/book/${id}`
+          },
+          {
+            collection: this.categories,
+            title: "Categories",
+            routeFactory: (id) => `/v2/category/${id}`
+          },
+          {
+            collection: this.difficulties,
+            title: "Difficulties",
+            routeFactory: (id) => `/v2/difficulty/${id}`
+          },
+        ]
+      }
+    }
   }
 </script>
 
